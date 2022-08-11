3aca49461dee486352b6a9f2bb7ff8fb94189ceb
dianabukliv <dianabukliv@gmail.com>
2022-08-12 01:01:37 +0300

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Post from './Post/Post';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const max_image = "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper.png";
+const ukraineImage = "https://upload.wikimedia.org/wikipedia/commons/5/5e/Flag_of_Ukraine.jpg";
+
+
+const App = () => {
+   return (
+      <div>
+         <Post author={{
+            name: "Буклів Максим",
+            photo: max_image,
+            nickname: "@buklivdiana"
+         }}
+            content="Слава Україні!"
+            image={ukraineImage}
+            date={"11 лип."}
+         />
+         
+      </div>
+   )
 }
 
 export default App;
